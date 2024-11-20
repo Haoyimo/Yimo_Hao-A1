6f7ad5d24aaad474c9518f7074ea3f200b604608
Haoyimo <1187908404@qq.com>
2024-11-20 17:03:31 +0800

    Update AssignmentOne.java

--- src/AssignmentOne.java
+++ src/AssignmentOne.java
@@ -3,13 +3,13 @@ public class AssignmentOne {
         // 第3部分 - 使用类和对象
         
         // 创建三个全科医生对象
-        GeneralPractitioner gp1 = new GeneralPractitioner(20040140, "Dr.Li", 40, "General Practitioner");
-        GeneralPractitioner gp2 = new GeneralPractitioner(20040122, "Dr.Liu", 22, "General Practitioner");
-        GeneralPractitioner gp3 = new GeneralPractitioner(20040159, "Dr.Zhang", 59, "General Practitioner");
+        GeneralPractitioner gp1 = new GeneralPractitioner(20040140, "Dr.Li", 40, "General Practitioner", "jgoisdkh");
+        GeneralPractitioner gp2 = new GeneralPractitioner(20040122, "Dr.Liu", 22, "General Practitioner","jgoisdkh");
+        GeneralPractitioner gp3 = new GeneralPractitioner(20040159, "Dr.Zhang", 59, "General Practitioner","jgoisdkh");
         
         // 创建两个其他类型的卫生专业人员对象
-        Nurse nurse1 = new Nurse(20040129, "Dr.Hao", 29, "Nurse");
-        Nurse nurse2 = new Nurse(20040136, "Dr.Zhai", 36, "Nurse");
+        Nurse nurse1 = new Nurse(20040129, "Dr.Hao", 29, "Nurse","jgoisdkh");
+        Nurse nurse2 = new Nurse(20040136, "Dr.Zhai", 36, "Nurse","jgoisdkh");
         
         // 打印所有卫生专业人员的详细信息
         gp1.printDetails();
@@ -21,6 +21,7 @@ public class AssignmentOne {
         nurse1.printDetails();
         System.out.println(""); // 空行分隔
         nurse2.printDetails();
+        System.out.println("------------------------------");// 作业要求
     }
 }
 
