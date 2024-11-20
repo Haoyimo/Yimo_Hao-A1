e544e2c6fc8a561a030c13a6cdc0ecdbcaebfe3f
Haoyimo <1187908404@qq.com>
2024-11-20 21:16:58 +0800

    Update HealthProfessional.java

--- src/HealthProfessional.java
+++ src/HealthProfessional.java
@@ -1,30 +1,30 @@
 public class HealthProfessional {
-    private int id;          // 设置变量
+    private int id;          //Set variables
     private String name;    
     private int age;        
 
-    // 构造函数
+    //Constructor function
     public HealthProfessional() {
         this.id = 0;
         this.name = "";
         this.age = 0; 
     }
 
-    // 初始化所有变量的构造函数
+    //Initialize the constructor of all variables
     public HealthProfessional(int id, String name, int age) {
         this.id = id;
         this.name = name;
         this.age = age;
     }
 
-    // 打印所有实例变量的方法
+    //Method for printing all instance variables
     public void printDetails() {
         System.out.println("ID: " + id);
         System.out.println("Name: " + name);
         System.out.println("Age: " + age);
     }
 
-    // 使用getter和setter来设置和获取变量
+    //Using Getters and Setters to Set and Retrieve Variables
     public int getId() {
         return id;
     }
