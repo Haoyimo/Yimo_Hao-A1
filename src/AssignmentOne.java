e65ca82bde132d260b751172b0771d211602814b
Haoyimo <1187908404@qq.com>
2024-11-20 16:09:56 +0800

    Update AssignmentOne.java

--- src/AssignmentOne.java
+++ src/AssignmentOne.java
@@ -1,7 +1,26 @@
 public class AssignmentOne {
     public static void main(String[] args) {
-        // 程序从这里开始执行
-        System.out.println("Hello, World!");
+        // 第3部分 - 使用类和对象
+        
+        // 创建三个全科医生对象
+        GeneralPractitioner gp1 = new GeneralPractitioner(20040140, "Dr.Li", 40, "General Practitioner");
+        GeneralPractitioner gp2 = new GeneralPractitioner(20040122, "Dr.Liu", 22, "General Practitioner");
+        GeneralPractitioner gp3 = new GeneralPractitioner(20040159, "Dr.Zhang", 59, "General Practitioner");
+        
+        // 创建两个其他类型的卫生专业人员对象
+        Nurse nurse1 = new Nurse(20040129, "Dr.Hao", 29, "Nurse");
+        Nurse nurse2 = new Nurse(20040136, "Dr.Zhai", 36, "Nurse");
+        
+        // 打印所有卫生专业人员的详细信息
+        gp1.printDetails();
+        System.out.println(""); // 空行分隔
+        gp2.printDetails();
+        System.out.println(""); // 空行分隔
+        gp3.printDetails();
+        System.out.println(""); // 空行分隔
+        nurse1.printDetails();
+        System.out.println(""); // 空行分隔
+        nurse2.printDetails();
     }
 }
 
